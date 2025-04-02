97d97b5a2733e55ef658d2b95a7f9eb7795864b7
Toby Carter <toby3carter@gmail.com>
2025-04-02 11:14:32 +0100

    3 passing tests, ive created the logic for the first three scenarios in the triangle spec file

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -2,8 +2,23 @@
 
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c 
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in triangle_spec. No need to change this code.
 class TriangleError < StandardError
+
 end
+
+# def triangle(a, b, c)
+#   if a <= 0 || b <= 0 || c <= 0
+#     raise TriangleError, "All sides must be greater than 0"
+#   end
+# end
+# 
