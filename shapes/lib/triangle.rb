83314c3414c418f1922b1c807c20be7d060a2dbc
Toby Carter <toby3carter@gmail.com>
2025-05-02 16:36:27 +0100

    simplified trinagle.rb answers

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  # WRITE THIS CODE
-  if a == b && b == c
+  
+  sides = [a, b, c].sort
+  
+  if sides.first <= 0
+    raise TriangleError, "All sides must be greater than 0"
+  end
+
+  if sides[0] + sides[1] <= sides[2]
+    raise TriangleError, "The sum of the lengths of any two sides must be greater than the length of the third side"
+  end
+
+  if sides[0] == sides[1] && sides[1] == sides[2]
     :equilateral
-  elsif a == b || b == c || a == c 
+  elsif sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2] 
     :isosceles
   else
     :scalene
@@ -13,12 +23,4 @@ end
 
 # Error class used in triangle_spec. No need to change this code.
 class TriangleError < StandardError
-
 end
-
-# def triangle(a, b, c)
-#   if a <= 0 || b <= 0 || c <= 0
-#     raise TriangleError, "All sides must be greater than 0"
-#   end
-# end
-# 
