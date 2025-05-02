cec203fca10ff5a25946d9e275cbdea878e88cf6
Toby Carter <toby3carter@gmail.com>
2025-05-02 16:45:01 +0100

    start made on quadrilateral logic on quadrilateral.rb file

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  # WRITE THIS CODE
+  angles = [a, b, c, d].sort
+
+  if angles.any? { |angle| angle <= 0 }
+    raise QuadrilateralError, "All angles must be greater than 0"
+  end
+  if angles.all? { |angle| angle == 90 }
+    [:square, :rectangle]
+  end
+
 end
 
 # Error class used in quadrilateral_spec. No need to change this code.
