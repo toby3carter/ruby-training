5107834f44a7076d77b5125497443736215cce79
Toby Carter <toby3carter@gmail.com>
2025-05-20 10:45:03 +0100

    feature: Continued on with quadrilateral spec, but got stuck with failing tests

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
+  
   angles = [a, b, c, d].sort
 
-  if angles.any? { |angle| angle <= 0 }
-    raise QuadrilateralError, "All angles must be greater than 0"
-  end
-  if angles.all? { |angle| angle == 90 }
-    [:square, :rectangle]
+  if angles[0] == angles[1] && angles[2] == angles[3]
+    [:parallelogram, :rhombus]
   end
 
+  if angles.any? { |angle| angle <= 0 } # When any angle is lower than or equal to 0 (This completes two scenarios from the spec file)
+    raise QuadrilateralError, "All angles must be greater than 0"
+  end
+
+  if angles[0] + angles[1] + angles[2] + angles[3] != 360 # When the sum of the angles is not equal to 360
+    raise QuadrilateralError, "The sum of the angles must equal 360"
+  end
 end
 
 # Error class used in quadrilateral_spec. No need to change this code.
