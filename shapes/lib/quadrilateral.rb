546b2d1ab1b5d093bf01c4eee285125142d7713d
Toby Carter <toby3carter@gmail.com>
2025-05-23 14:29:36 +0100

    feature: added new method defining if there are two pairs of equal angles

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -4,10 +4,6 @@ def quadrilateral(a, b, c, d)
   
   angles = [a, b, c, d].sort
 
-  if angles[0] == angles[1] && angles[2] == angles[3]
-    [:parallelogram, :rhombus]
-  end
-
   if angles.any? { |angle| angle <= 0 } # When any angle is lower than or equal to 0 (This completes two scenarios from the spec file)
     raise QuadrilateralError, "All angles must be greater than 0"
   end
@@ -15,6 +11,20 @@ def quadrilateral(a, b, c, d)
   if angles[0] + angles[1] + angles[2] + angles[3] != 360 # When the sum of the angles is not equal to 360
     raise QuadrilateralError, "The sum of the angles must equal 360"
   end
+
+  if angles.uniq == [90]
+    return[:square, :rectangle]
+  elsif two_pairs_of_equal_angles?(angles)
+    return[:parallelogram, :rhombus]
+  else
+    return[:quadrilateral]
+  end
+end
+
+def two_pairs_of_equal_angles?(angles)
+  freq = Hash.new(0)
+  angles.each { |a| freq[a] += 1 }
+  freq.values.sort == [2, 2]
 end
 
 # Error class used in quadrilateral_spec. No need to change this code.
